9a9dda2fe351a40f13550e929bbfbe605c9e7525
whycwen <8720whyc@gmail.com>
2017-08-01 17:30:44 +0800

    new a file first commit

---
+++ ddd.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2  
+# encoding: utf-8  
+
+""" 
+@version: v1.0 
+@author: whyc
+@license: Apache Licence  
+@contact: 8720whyc@gmail.com
+@site:  
+@software: PyCharm 
+@file: ddd.py 
+@time: 17-8-1 下午5:27 
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass  
